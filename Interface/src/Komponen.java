3f5675fd82d4082f998c34ec6e56de15cd405519
Muhammad Hafizh Syabana <101510248+HafizhSyabana@users.noreply.github.com>
2022-12-15 09:05:13 +0700

    Add files via upload

---
+++ Interface/src/Komponen.java
@@ -0,0 +1,10 @@
+public interface Komponen {
+
+    //Secara Implisit Variable tersebut Bersifat Public Static dan Final
+    double kecepatan = 160.0;
+    String model = "Lamborghini";
+
+    //Secara Implisit Method Tersebut Bersifat Public dan Abstrak
+    void mesin();
+    void design();
+}
